8de3a7115c472205a38b0dd9c19b087a773ae349
yoonseungmin <sunytony@naver.com>
2020-07-14 01:19:48 +0900

    Update makefile

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 all: add-nbo
 
-add-nbo: add-nbo.o main.o
+add-nbo: main.o add-nbo.o
 	gcc -o add-nbo main.o add-nbo.o
 
 main.o: main.c
-	gcc -o main.o main.c
+	gcc -c -o main.o main.c
 
 add-nbo.o: add-nbo.c
-	gcc -o add-nbo.o add-nbo.c
+	gcc -c -o add-nbo.o add-nbo.c
 
 clean:
 	rm -f add-nbo *.o
--- add-nbo.c
+++ add-nbo.c
@@ -1,4 +1,5 @@
-#include<stdint.h>
+#include <stdint.h>
+#include <stdio.h>
 #include "add-nbo.h"
 
 uint32_t my_ntohl(uint32_t a){
@@ -12,7 +13,7 @@ void add_nbo(uint32_t a, uint32_t b){
 	uint32_t num1 = my_ntohl(a);
 	uint32_t num2 = my_ntohl(b);
 	uint32_t sum = num1 + num2;
-	printf("%d(%x) + %d(%x) = %d(%x)", num1, num1, num2, num2, sum, sum);
+	printf("%d(%x) + %d(%x) = %d(%x)\n", num1, num1, num2, num2, sum, sum);
 
 	return;
 }
